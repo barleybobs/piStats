97afa0fd25a28b46d06e3bad3438c254afb42156
barleybobs <53116390+barleybobs@users.noreply.github.com>
2020-02-18 18:31:42 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 cd /home/pi
 mkdir piStats
 cd /piStats
-git clone [url]
+git clone https://github.com/barleybobs/piStats
 sudo pip install psutil
 sudo pip install pathlib
 sudo pip install termcolor
