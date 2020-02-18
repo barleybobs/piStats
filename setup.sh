73ca05db23dc57789732c37113a866886e90983e
barleybobs <53116390+barleybobs@users.noreply.github.com>
2020-02-18 18:29:28 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /home/pi
+mkdir piStats
+cd /piStats
+git clone [url]
+sudo pip install psutil
+sudo pip install pathlib
+sudo pip install termcolor
+sudo pip install hurry.filesize
+sudo apt-get install xterm
+sudo apt-get install unclutter
